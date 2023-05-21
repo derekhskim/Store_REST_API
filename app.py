1ea9999516d24f987af462302ecb69c7f1b2bbc1
[treasure3210] <treasure3210@gmail.com>
2023-05-20 21:07:32 -0600

    implement create store POST /store

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -17,3 +17,14 @@ stores = [
 @app.get("/store")
 def get_stores():
     return {"stores": stores}
+
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
