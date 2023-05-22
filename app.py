d100c2909da48d673ab859f4b2aeeabf35afe4da
[treasure3210] <treasure3210@gmail.com>
2023-05-22 16:24:17 -0600

    Modify db setup and edit endpoints

--- app.py
+++ app.py
@@ -1,59 +1,56 @@
+import uuid
 from flask import Flask, request
+from db import items, stores
 
 app = Flask(__name__)
-
-# Sample/Default data
-stores = [
-    {
-        "name": "My Store",
-        "items": [
-            {
-                "name": "Chair",
-                "price": 15.99
-            }
-        ]
-    }
-]
 
 # GET - Retrieve All Stores and Their Items
 @app.get("/store")
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
+
+# GET - Get All Items
+@app.get("/item") 
+def get_items():
+    return {"items": list(items.values())}
 
 # GET - Retrieve a Particular Store
-@app.get("/store/<string:name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-    return {"message": "Store not found"}, 404
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {"message": "Store not found"}, 404
 
 # GET - Retrieve Items from a Store
-@app.get("/store/<string:name>/item")
-def get_item(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"items": store["items"]}
-    return {"message": "Store not found"}, 404
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try: 
+        return items[item_id]
+    except KeyError:
+        return {"message": "Store not found"}, 404
 
 # POST - Create a Store
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    return new_store, 201
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
 
 # POST - Create an Item
-@app.post("/store/<string:name>/item")
-def create_item(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": request_data["name"], "price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item, 201
-    return {"message": "Store not found"}, 404
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
+    
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item, 201
 
 # port 5000 was glitchy - localhost forced to use port 5001
 if __name__ == '__main__':
