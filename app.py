f8ed50a9c9ac09495af61551247306a2eb9b8ffb
[treasure3210] <treasure3210@gmail.com>
2023-05-23 23:23:34 -0600

    Add few more endpoints (delete/update store&items)

--- app.py
+++ app.py
@@ -10,11 +10,6 @@ app = Flask(__name__)
 def get_stores():
     return {"stores": list(stores.values())}
 
-# GET - Get All Items
-@app.get("/item") 
-def get_items():
-    return {"items": list(items.values())}
-
 # GET - Retrieve a Particular Store
 @app.get("/store/<string:store_id>")
 def get_store(store_id):
@@ -22,14 +17,6 @@ def get_store(store_id):
         return stores[store_id]
     except KeyError:
         abort(404, message="Store not found.")
-
-# GET - Retrieve Items from a Store
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try: 
-        return items[item_id]
-    except KeyError:
-        abort(404, message="Item not found.")
 
 # POST - Create a Store
 @app.post("/store")
@@ -49,6 +36,28 @@ def create_store():
     stores[store_id] = store
     return store, 201
 
+# DELETE - Delete a Store
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store was successfully deleted."}
+    except KeyError:
+        abort(404, message="Store not found.")
+
+# GET - Get All Items
+@app.get("/item") 
+def get_all_items():
+    return {"items": list(items.values())}
+
+# GET - Retrieve Specific Item
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try: 
+        return items[item_id]
+    except KeyError:
+        abort(404, message="Item not found.")
+
 # POST - Create an Item
 @app.post("/item")
 def create_item():
@@ -63,7 +72,7 @@ def create_item():
             message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload."
         )
 
-    for item in item.values():
+    for item in items.values():
         if (
             item_data["name"] == item["name"]
             and item_data["store_id"] == item["store_id"]
@@ -77,6 +86,30 @@ def create_item():
     items[item_id] = item
 
     return item, 201
+
+# DELETE - Delete an Item
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item was successfully deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
+
+# UPDATE - Update an Item
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.")
+
+    try:
+        item = items[item_id]
+        item |= item_data
+
+        return item
+    except KeyError:
+        abort(404, message="Item not found.")
 
 # port 5000 was glitchy - localhost forced to use port 5001
 if __name__ == '__main__':
