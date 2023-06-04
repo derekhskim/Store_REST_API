84f8c6f2bb09f7db9fea66f5f5a2f74b1573aa94
[treasure3210] <treasure3210@gmail.com>
2023-06-03 18:21:55 -0600

    configure flask-sqlalchemy with flask app

--- app.py
+++ app.py
@@ -1,26 +1,40 @@
+import os
+
 from flask import Flask
 from flask_smorest import Api
+
+from db import db
+import models
 
 from resources.item import blp as ItemBluePrint
 from resources.store import blp as StoreBluePrint
 
+def create_app(db_url=None):
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    app.config["PROPAGATE_EXEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    
+    db.init_app(app)
 
+    api = Api(app)
 
-app.config["PROPAGATE_EXEPTIONS"] = True
-app.config["API_TITLE"] = "Stores REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    with app.app_context():
+        db.create_all()
 
-api = Api(app)
+    api.register_blueprint(ItemBluePrint)
+    api.register_blueprint(StoreBluePrint)
 
-api.register_blueprint(ItemBluePrint)
-api.register_blueprint(StoreBluePrint)
+    return app
 
 # port 5000 was glitchy - localhost forced to use port 5001
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app = create_app
+    create_app.run(debug=True, port=5001)
--- resources/store.py
+++ resources/store.py
@@ -3,7 +3,6 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
-from db import stores
 from schemas import StoreSchema
 
 
