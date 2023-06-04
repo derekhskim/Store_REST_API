506ac20f0af4667704d59622c49605f3d5024cb2
[treasure3210] <treasure3210@gmail.com>
2023-06-03 19:41:19 -0600

    update schema and allow new create item put

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def create_app(db_url=None):
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    
+
     db.init_app(app)
 
     api = Api(app)
--- schemas.py
+++ schemas.py
@@ -15,6 +15,7 @@ class PlainStoreSchema(Schema):
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
+    store_id = fields.Int()
 
 
 class ItemSchema(PlainItemSchema):
