e102a4ae1b5b1d683f1280ca7727b7fae334924d
[treasure3210] <treasure3210@gmail.com>
2023-06-07 14:23:21 -0600

    Add/modify email templates and rq at bg use

--- tasks.py
+++ tasks.py
@@ -1,19 +1,28 @@
 import os
 import requests
+import jinja2
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DOMAIN = os.getenv("MAILGUN_DOMAIN")
+template_loader = jinja2.FileSystemLoader("templates")
+template_env = jinja2.Environment(loader=template_loader)
 
-def send_simple_message(to, subject, body):
+
+def render_template(template_filename, **context):
+    return template_env.get_template(template_filename).render(**context)
+
+
+def send_simple_message(to, subject, body, html):
     return requests.post(
 		f"https://api.mailgun.net/v3/{DOMAIN}/messages",
 		auth=("api", os.getenv("MAILGUN_API_KEY")),
 		data={"from": f"Noobietubie <mailgun@{DOMAIN}>",
 			"to": [to],
 			"subject": subject,
-			"text": body
+			"text": body,
+            "html": html
         }
     )
 
@@ -23,4 +32,5 @@ def send_user_registration_email(email, username):
         email,
         "Successfully signed up!",
         f"Hi {username}! You have successfully signed up to the Stores REST API.",
+        render_template("email/action.html", username=username)
     )
