9fdcae2b9c187b017de5c60f9eee61c3b9078926
[treasure3210] <treasure3210@gmail.com>
2023-06-06 22:06:27 -0600

    populate and consume the task queue w/ rq

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
+import redis
 
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from dotenv import load_dotenv
+from rq import Queue
 
 from db import db
 from blocklist import BLOCKLIST
@@ -18,6 +20,10 @@ def create_app(db_url=None):
     app = Flask(__name__)
     load_dotenv()
 
+    connection = redis.from_url(
+        os.getenv("REDIS_URL")
+    )
+    app.queue = Queue("emails", connection=connection)
     app.config["PROPAGATE_EXEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
--- resources/user.py
+++ resources/user.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from flask import current_app
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
@@ -10,22 +11,10 @@ from db import db
 from blocklist import BLOCKLIST
 from models import UserModel
 from schemas import UserSchema, UserRegisterSchema
+from tasks import send_user_registration_email
 
 
 blp = Blueprint("Users", "users", description="Operations on users")
-
-
-def send_simple_message(to, subject, body):
-    domain = os.getenv("MAILGUN_DOMAIN")
-    return requests.post(
-		f"https://api.mailgun.net/v3/{domain}/messages",
-		auth=("api", os.getenv("MAILGUN_API_KEY")),
-		data={"from": f"Noobietubie <mailgun@{domain}>",
-			"to": [to],
-			"subject": subject,
-			"text": body
-        }
-    )
 
 
 @blp.route("/register")
@@ -48,11 +37,7 @@ class UserRegister(MethodView):
         db.session.add(user)
         db.session.commit()
 
-        send_simple_message(
-            to=user.email,
-            subject="Successfully signed up!",
-            body=f"Hi {user.username}! You have successfully signed up to the Stores REST API!"
-        )
+        current_app.queue.enqueue(send_user_registration_email, user.email, user.username)
         
         return {"message": "User created successfully."}, 201
     
