28759bfc44184067346eb1c06da5e73b3727a7c9
[treasure3210] <treasure3210@gmail.com>
2023-06-06 19:25:47 -0600

    configure email for registration: mailgun

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -3,7 +3,8 @@
 ## How to run the Dockerfile locally
 
 ```
-docker run -dp 5000:5000 -w /app -v "$(pwd):/app" site-flask sh -c "flask run --host 0.0.0.0"
+docker build -t rest-api-email .
+docker run -p 5000:5000 -w /app -v "$(pwd):/app" rest-api-email sh -c "flask run --host 0.0.0.0"
 ```
 
 ## .env Not Available in Github repository
--- resources/user.py
+++ resources/user.py
@@ -1,27 +1,58 @@
+import os
+import requests
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
 from flask_jwt_extended import create_access_token, create_refresh_token,  get_jwt_identity, jwt_required, get_jwt
+from sqlalchemy import or_
 
 from db import db
 from blocklist import BLOCKLIST
 from models import UserModel
-from schemas import UserSchema
+from schemas import UserSchema, UserRegisterSchema
 
 
 blp = Blueprint("Users", "users", description="Operations on users")
 
 
+def send_simple_message(to, subject, body):
+    domain = os.getenv("MAILGUN_DOMAIN")
+    return requests.post(
+		f"https://api.mailgun.net/v3/{domain}/messages",
+		auth=("api", os.getenv("MAILGUN_API_KEY")),
+		data={"from": f"Noobietubie <mailgun@{domain}>",
+			"to": [to],
+			"subject": subject,
+			"text": body
+        }
+    )
+
+
 @blp.route("/register")
 class UserRegister(MethodView):
-    @blp.arguments(UserSchema)
+    @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
-        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
-            abort(409, message="A user with that username already exists.")
+        if UserModel.query.filter(
+            or_(
+                UserModel.username == user_data["username"],
+                UserModel.email == user_data["email"]
+                )
+            ).first():
+                abort(409, message="A user with that username or email already exists.")
 
-        user = UserModel(username=user_data["username"], password=pbkdf2_sha256.hash(user_data["password"]))
+        user = UserModel(
+            username=user_data["username"],
+            email=user_data["email"],
+            password=pbkdf2_sha256.hash(user_data["password"])
+        )
         db.session.add(user)
         db.session.commit()
+
+        send_simple_message(
+            to=user.email,
+            subject="Successfully signed up!",
+            body=f"Hi {user.username}! You have successfully signed up to the Stores REST API!"
+        )
         
         return {"message": "User created successfully."}, 201
     
